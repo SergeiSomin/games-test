04cd5a69e1738d9357bf23b0a911281b27335eb6
SergeiSomin <sominsergei@gmail.com>
2023-10-21 15:12:18 +0200

    Add base deck structure and tops rendering

--- src/pages/CardsPage.ts
+++ src/pages/CardsPage.ts
@@ -1,18 +1,20 @@
-import { Container, Point, Sprite } from "pixi.js";
+import { Container, Point, Sprite, Texture } from "pixi.js";
 import { IViewportData } from "../Viewport";
 import { BasePage } from "./BasePage";
-import { randomIntRange, zeroPad } from "../utils";
+import { arrayFill, randomIntRange, zeroPad } from "../utils";
 
 class Card {
 
+	private _type: string;
 	private _sprite: Sprite;
 
 	constructor(container: Container) {
+		this._type = "";
 		this._sprite = this.createSprite(container);
 	}
 
 	private createSprite(container: Container) {
-		const sprite = Sprite.from(`d${zeroPad(randomIntRange(0, 13).toString(), 2)}`);
+		const sprite = new Sprite();
 		sprite.anchor.set(0.5);
 		container.addChild(sprite);
 		return sprite;
@@ -26,23 +28,51 @@ class Card {
 		this._sprite.x = x;
 		this._sprite.y = y;
 	}
+
+	setType(type: string) {
+		this._type = type;
+		this._sprite.texture = Texture.from(type);
+	}
+
+	getType() {
+		return this._type;
+	}
 }
+
+
+const DECK_MAX_VISIBLE_CARDS = 7;
+const DECK_TOPS_OFFSET = 260;
 
 class Deck {
 
+	private _tops: Sprite;
 	private _visibleCards: Card[];
 
-	constructor(container: Container) {
-		this._visibleCards = [
-			new Card(container),
-			new Card(container),
-			new Card(container),
-			new Card(container)
-		];
+	constructor(container: Container, cards: string[]) {
+		this._visibleCards = arrayFill<Card>(DECK_MAX_VISIBLE_CARDS, (index) => this.createCard(container, cards[index]));
+		this._tops = Sprite.from("tops");
+		container.addChild(this._tops);
+		this._tops.anchor.set(0.5, 1);
+	}
+
+	private createCard(container: Container, type: string) {
+		const card = new Card(container);
+		card.setType(type);
+		return card;
 	}
 
 	setPosition(x: number, y: number) {
-		this._visibleCards.forEach((card, index) => card.setPosition(x, y + index * 50));
+		this._tops.x = x;
+		this._tops.y = y - DECK_TOPS_OFFSET;
+		this._visibleCards.forEach((card, index) => card.setPosition(x, y + index * 20));
+	}
+
+	removeCard() {
+
+	}
+
+	addCard() {
+
 	}
 }
 
@@ -54,11 +84,17 @@ export class CardsPage extends BasePage {
 	private _deck1?: Deck;
 	private _deck2?: Deck;
 
-	show(): void {
-		this._deck1 = new Deck(this.container);
-		this._deck2 = new Deck(this.container);
+	// private _movingCards: Card[];
 
-		super.show();
+	private getRandomCardType() {
+		return `d${zeroPad(randomIntRange(1, 13).toString(), 2)}`;
+	}
+
+	show(): void {
+		this._deck1 = new Deck(this.container, arrayFill(144, () => this.getRandomCardType()));
+		this._deck2 = new Deck(this.container, arrayFill(144, () => this.getRandomCardType()));
+
+		super.show();	
 	}
 
 	resize({centerX, centerY}: IViewportData): void {
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,3 @@
 export const randomIntRange = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
 export const zeroPad = (str: string, amount: number) => str.padStart(amount, "0");
+export const arrayFill = <T>(amount: number, creator: (index: number) => T) => new Array(amount).fill(0).map(creator);
