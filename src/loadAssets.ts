72aa83659638d49a89288e1ab579daa048f229bb
SergeiSomin <sominsergei@gmail.com>
2023-10-21 12:34:44 +0200

    Add loading and unloading of assets

---
+++ src/loadAssets.ts
@@ -0,0 +1,18 @@
+import { Assets } from "pixi.js";
+
+export enum AssetBundle {
+	CARDS = "cards",
+	MIX_TOOL = "mix-tool",
+	PARTICLES = "particles"
+}
+
+export const loadAssets = async (bundle: AssetBundle) => {
+	const response = await fetch("manifest.json");
+    const manifest = await response.json();
+	await Assets.init({manifest});
+	await Assets.loadBundle(bundle);
+}
+
+export const unloadAssets = async (bundle: AssetBundle) => {
+	await Assets.unloadBundle(bundle);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
 import './style.css'
+import { Application, Assets } from "pixi.js";
 
-console.log("Hello World");
+import { AssetBundle, loadAssets, unloadAssets } from './loadAssets';
+
+
+const init = async () => {
+	const app = new Application<HTMLCanvasElement>();
+	document.body.appendChild(app.view);
+
+	await loadAssets(AssetBundle.CARDS);
+	console.log(Assets.cache.get("d01"));
+
+	await unloadAssets(AssetBundle.CARDS);
+	console.log(Assets.cache.get("d01"));
+}
+
+
+init();
