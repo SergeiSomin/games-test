a484c49558dbe82f6e366da80668dd8a1bcb7120
SergeiSomin <sominsergei@gmail.com>
2023-10-21 17:58:30 +0200

    Add sortable children container only for moving cards

--- src/pages/CardsPage.ts
+++ src/pages/CardsPage.ts
@@ -1,8 +1,16 @@
 import gsap from "gsap";
-import { Container, IPoint, Point, Sprite, Texture } from "pixi.js";
+import { Container, Sprite, Text, Texture } from "pixi.js";
 import { IViewportData } from "../Viewport";
 import { BasePage } from "./BasePage";
 import { arrayFill, randomIntRange, zeroPad } from "../utils";
+
+const DECK_CENTER_OFFSET_X = 450;
+const DECK_CENTER_OFFSET_Y = -250;
+const DECK_CARD_MOVE_TIME = 2;
+const DECK_CARD_SPAWN_DELAY = 1;
+const DECK_MAX_VISIBLE_CARDS = 11;
+const DECK_TOPS_OFFSET = 260;
+const DECK_CARD_OFFSET = 45;
 
 class Card {
 
@@ -46,13 +54,7 @@ class Card {
 
 	setType(type: string) {
 		this._type = type;
-
-		if(type) {
-			this._sprite.visible = true;
-			this._sprite.texture = Texture.from(type);
-		} else {
-			this._sprite.visible = false;
-		}
+		this._sprite.texture = type ? Texture.from(type) : Texture.EMPTY;
 	}
 
 	getType() {
@@ -62,22 +64,39 @@ class Card {
 	isMoving() {
 		return this._isMoving;
 	}
+
+	setZIndex(zIndex: number) {
+		this._sprite.zIndex = zIndex;
+	}
 }
 
 
-const DECK_MAX_VISIBLE_CARDS = 7;
-const DECK_TOPS_OFFSET = 260;
-
 class Deck {
 
+
+	private _cardsNumberText: Text;
+
+	private _x: number;
+	private _y: number;
 	private _tops: Sprite;
 	private _visibleCards: Card[];
 	private _cards: string[];
-
+	private _cardsNumber: number = 0;
+	
 	constructor(container: Container) {
 		this._visibleCards = arrayFill<Card>(DECK_MAX_VISIBLE_CARDS, () => this.createCard(container, ""));
+		this._cardsNumberText = this.createCardsNumberText(container);
 		this._tops = this.createTops(container);
 		this._cards = [];
+		this._x = 0;
+		this._y = 0;
+		this._cardsNumber = 0;
+	}
+
+	private createCardsNumberText(container: Container) {
+		const text = new Text();
+		container.addChild(text);
+		return text;
 	}
 
 	private createCard(container: Container, type: string) {
@@ -95,15 +114,35 @@ class Deck {
 	}
 
 	private updateVisibleCards() {
-		this._cards
-			.slice(-DECK_MAX_VISIBLE_CARDS)
-			.forEach((cardType, index) => this._visibleCards[index].setType(cardType));
+		const visibleCards = this._cards.slice(-DECK_MAX_VISIBLE_CARDS);
+
+		this._visibleCards.forEach((card) => {
+			card.setType("");
+		});
+
+		visibleCards.forEach((cardType, index) => {
+			this._visibleCards[index].setType(cardType);
+			this._visibleCards[index].setPosition(this._x, this._y + index * DECK_CARD_OFFSET);
+		});
 	}
 
 	setPosition(x: number, y: number) {
+		this._x = x;
+		this._y = y;
 		this._tops.x = x;
 		this._tops.y = y - DECK_TOPS_OFFSET;
-		this._visibleCards.forEach((card, index) => card.setPosition(x, y + index * 20));
+		this._visibleCards.forEach((card, index) => card.setPosition(x, y + index * DECK_CARD_OFFSET));
+	}
+
+	increaseCardsNumber() {
+		this._cardsNumber += 1;
+	}
+
+	getAddPosition() {
+		return {
+			x: this._x,
+			y: Math.min(this._cardsNumber, DECK_MAX_VISIBLE_CARDS) * DECK_CARD_OFFSET
+		}
 	}
 
 	removeCard() {
@@ -115,10 +154,12 @@ class Deck {
 
 		this.updateVisibleCards();
 
+		this._cardsNumber -= 1;
+
 		return card;
 	}
 
-	addCard(type: string) {
+	addTopCard(type: string) {
 		this._cards.push(type);
 
 		if(this._cards.length > 7) {
@@ -129,14 +170,11 @@ class Deck {
 	}
 
 	getTopCardPosition() {
-		return this._visibleCards[this._visibleCards.length - 1].getPosition();
+		const index = Math.min(this._cardsNumber, DECK_MAX_VISIBLE_CARDS) - 1;
+		return this._visibleCards[index] ? this._visibleCards[index].getPosition() : {x: this._x, y: this._y};
 	}
 }
 
-
-const DECK_CENTER_OFFSET = 450;
-const DECK_CARD_MOVE_TIME = 2;
-const DECK_CARD_MOVE_DELAY = 1;
 
 export class CardsPage extends BasePage {
 
@@ -148,24 +186,40 @@ export class CardsPage extends BasePage {
 		return `d${zeroPad(randomIntRange(1, 13).toString(), 2)}`;
 	}
 
+	private spawnMovingCard(type: string) {
+		const cards = this._movingCards!;
+		const card = cards.shift()!;
+		cards.push(card);
+		cards.forEach((card, index) => card.setZIndex(index * 100));
+
+		card.setType(type);
+		return card;
+	}
+
 	show(): void {
 		super.show();
 
 		this._deck1 = new Deck(this.container);
 		this._deck2 = new Deck(this.container);
 
-		const maxMovingCards = Math.ceil(DECK_CARD_MOVE_TIME / DECK_CARD_MOVE_DELAY);
-		this._movingCards = arrayFill(maxMovingCards, () => new Card(this.container));
+		const movingCardsContainer = new Container();
+		movingCardsContainer.sortableChildren = true;
+		this.container.addChild(movingCardsContainer);
+
+		const maxMovingCards = Math.ceil(DECK_CARD_MOVE_TIME / DECK_CARD_SPAWN_DELAY) + 1;
+		this._movingCards = arrayFill(maxMovingCards, () => new Card(movingCardsContainer));
 
 		for(let i = 0; i < 144; i++) {
-			this._deck1.addCard(this.getRandomCardType());
+			this._deck1.addTopCard(this.getRandomCardType());
+			this._deck1.increaseCardsNumber();
 		}
 
-		this.startAnimation();
+		gsap.delayedCall(DECK_CARD_SPAWN_DELAY, () => this.moveAnimation());
 	}
 
 	async moveAnimation() {
 		const from = this._deck1!.getTopCardPosition();
+		this._deck2!.increaseCardsNumber()
 		const to = this._deck2!.getTopCardPosition();
 		const cardType = this._deck1!.removeCard();
 
@@ -173,26 +227,30 @@ export class CardsPage extends BasePage {
 			return;
 		}
 
-		const card = this._movingCards?.find((card: Card) => !card.isMoving())!;
-		card.setType(cardType);
+		gsap.delayedCall(DECK_CARD_SPAWN_DELAY, () => this.moveAnimation());
+
+		const card = this.spawnMovingCard(cardType);
 		await card.move(from, to, DECK_CARD_MOVE_TIME);
 		card.setType("");
 
-		this._deck2?.addCard(cardType!);
+		this._deck2?.addTopCard(cardType!);
+
 	}
 
-	startAnimation() {
-		this.moveAnimation();
-		gsap.delayedCall(1, () => this.startAnimation());
-	}
 
 	resize({centerX, centerY}: IViewportData): void {
 		if(this._deck1) {
-			this._deck1.setPosition(centerX - DECK_CENTER_OFFSET, centerY);
+			this._deck1.setPosition(
+				centerX - DECK_CENTER_OFFSET_X,
+				centerY + DECK_CENTER_OFFSET_Y
+			);
 		}
 
 		if(this._deck2) {
-			this._deck2.setPosition(centerX + DECK_CENTER_OFFSET, centerY);
+			this._deck2.setPosition(
+				centerX + DECK_CENTER_OFFSET_X,
+				centerY + DECK_CENTER_OFFSET_Y
+			);
 		}
 	}
 }
